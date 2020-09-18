cd981e5d3de6efd999b8fe9beca39e39c76adf85
Shaikot007 <shaikot999@gmail.com>
2020-09-18 19:38:37 +0600

    Updates

---
+++ precache-manifest.6d9b10aa42725eeaf02ebc7879468eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65a1419d63271207c0b2ba98833fee9",
+    "url": "/Drum-machine/index.html"
+  },
+  {
+    "revision": "eb149fe6552c5146bf3f",
+    "url": "/Drum-machine/static/css/main.379631ae.chunk.css"
+  },
+  {
+    "revision": "d6f0319ae71494d37fdb",
+    "url": "/Drum-machine/static/js/2.3c1b4878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drum-machine/static/js/2.3c1b4878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb149fe6552c5146bf3f",
+    "url": "/Drum-machine/static/js/main.c705ca65.chunk.js"
+  },
+  {
+    "revision": "e80790fa3c434aa25bfc",
+    "url": "/Drum-machine/static/js/runtime-main.95518db7.js"
+  }
+]);
